1d65d1959ed4524f7d60e8bae85ea6315e6aa38a
knowave <kwaktaemin98@gmail.com>
2024-02-07 13:21:29 +0900

    user entity 정의

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  userId: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  nickname: string;
+
+  @Column({ nullable: true })
+  sido: string;
+
+  @Column({ length: 300, default: '경기', nullable: true })
+  profileImage: string;
+}
