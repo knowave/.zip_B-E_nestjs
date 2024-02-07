5d8d179aabf8212f535aef0427a71ef33c7661c2
knowave <kwaktaemin98@gmail.com>
2024-02-07 14:43:45 +0900

    add apartment moudle

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { MysqlModule } from './mysql/mysql.module';
+import { ApartmentModule } from './apartment/apartment.module';
+import { ApartmentService } from './apartment/apartment.service';
+import { ApartmentController } from './apartment/apartment.controller';
 
 @Module({
-  imports: [UserModule, AuthModule, MysqlModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, AuthModule, MysqlModule, ApartmentModule],
+  controllers: [AppController, ApartmentController],
+  providers: [AppService, ApartmentService],
 })
 export class AppModule {}
