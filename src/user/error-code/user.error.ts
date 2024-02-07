49e4ac978115ef3f62208462d239162e8ed2cc58
knowave <kwaktaemin98@gmail.com>
2024-02-07 14:08:16 +0900

    EXIST_EMAIL_BAD_REQUEST

--- src/user/error-code/user.error.ts
+++ src/user/error-code/user.error.ts
@@ -4,3 +4,8 @@ export const USER_NOT_FOUND: CommonError = {
   code: 'USER_NOT_FOUND',
   message: '존재하는 사용자가 없습니다.',
 };
+
+export const EXIST_EMAIL_BAD_REQUEST: CommonError = {
+  code: 'EXIST_EMAIL_BAD_REQUEST',
+  message: '다름 이메일을 사용해주세요.',
+};
