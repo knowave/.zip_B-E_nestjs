ddc566cbf24d62ff03352d8641522da8f6f88ccf
knowave <kwaktaemin98@gmail.com>
2024-02-07 13:59:58 +0900

    Add token

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -5,18 +5,26 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   userId: string;
 
-  @Column()
+  @Column({ comment: '사용자 이메일' })
   email: string;
 
-  @Column({ select: false })
+  @Column({ comment: '사용자 비밀번호', select: false })
   password: string;
 
-  @Column()
+  @Column({ comment: '사용자 닉네임' })
   nickname: string;
 
-  @Column({ nullable: true })
-  sido: string;
+  @Column({ comment: '사용자 토큰', nullable: true })
+  token?: string;
 
-  @Column({ length: 300, default: '경기', nullable: true })
-  profileImage: string;
+  @Column({ comment: '관심지역', nullable: true })
+  preferredRegion?: string;
+
+  @Column({
+    comment: '프로필 이미지',
+    length: 300,
+    default: '경기',
+    nullable: true,
+  })
+  profileImage?: string;
 }
