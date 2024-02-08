2372e74793d1d4e52e2cb3639d5a0f919db0b8f1
knowave <kwaktaemin98@gmail.com>
2024-02-08 13:50:16 +0900

    add login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CurrentUser } from './decorator/current-user.decorator';
+import { User } from 'src/user/entities/user.entity';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@CurrentUser() user: User) {
+    const accessToken = await this.authService.createAccessToken(user);
+    const refreshToken = await this.authService.createRefreshToken(user);
+
+    return { accessToken, refreshToken };
+  }
+}
