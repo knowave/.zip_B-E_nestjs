f35d4201e8553cb4e75c467c4677bcc50c01ba7f
knowave <kwaktaemin98@gmail.com>
2024-02-13 18:52:08 +0900

    좋아요 진행중

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { LikeRepository } from './like.repository';
+import { User } from 'src/user/entities/user.entity';
+import { Like } from './entities/like';
 
 @Injectable()
-export class LikeService {}
+export class LikeService {
+  constructor(private likeRepository: LikeRepository) {}
+
+  async createLike(likeId: string, user: User) {
+    const like = await this.likeRepository.getLikeByIdAndUserId(
+      likeId,
+      user.userId,
+    );
+
+    if (!like) {
+      const createLike = new Like({
+        user,
+      });
+    }
+  }
+}
