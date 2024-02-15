1b259837347dccd7181dadf3acd4afff74b2c49f
knowave <kwaktaemin98@gmail.com>
2024-02-15 13:18:40 +0900

    Add Join pub notice

--- src/like/entities/like.ts
+++ src/like/entities/like.ts
@@ -1,6 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { PrivateApt } from 'src/private-apt/entities/private-apt.entity';
+import { PubNotice } from 'src/pub-notice/entities/pub-notice.entity';
 
 @Entity({ name: 'likes' })
 export class Like {
@@ -12,4 +13,9 @@ export class Like {
 
   @ManyToOne(() => PrivateApt, { onDelete: 'CASCADE' })
   privateApt: PrivateApt;
+
+  @ManyToOne(() => PubNotice, (pubNotice) => pubNotice.likes, {
+    onDelete: 'CASCADE',
+  })
+  pubNotice: PubNotice;
 }
