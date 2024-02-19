a51adceacd4bee19a665f473caaf802e6565ec1d
knowave <kwaktaemin98@gmail.com>
2024-02-19 15:38:22 +0900

    createLike

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -2,21 +2,35 @@ import { Injectable } from '@nestjs/common';
 import { LikeRepository } from './like.repository';
 import { User } from 'src/user/entities/user.entity';
 import { Like } from './entities/like';
+import { CreateLikeDto } from './dto/create-like.dto';
+import { PrivateAptRepository } from 'src/private-apt/private-apt.repository';
 
 @Injectable()
 export class LikeService {
-  constructor(private likeRepository: LikeRepository) {}
+  constructor(
+    private readonly likeRepository: LikeRepository,
+    private readonly privateAptRepository: PrivateAptRepository,
+  ) {}
 
-  async createLike(likeId: string, user: User) {
-    const like = await this.likeRepository.getLikeByIdAndUserId(
+  async createLike(createLikeDto: CreateLikeDto) {
+    const { likeId, userId, pblancNo, panId } = createLikeDto;
+
+    const like = await this.likeRepository.getLikeByIdAndUserId({
       likeId,
-      user.userId,
+      userId,
+      pblancNo,
+      panId,
+    });
+
+    const privateApt = await this.privateAptRepository.getPrivateAptByPblancNo(
+      pblancNo,
     );
 
     if (!like) {
-      const createLike = new Like({
-        user,
-      });
+      if (privateApt) {
+        const createLike = new Like();
+        privateApt.likes.push(createLike);
+      }
     }
   }
 }
