3add31f176469129cc737615ef72e7e4aa604016
knowave <kwaktaemin98@gmail.com>
2024-02-19 15:38:14 +0900

    Add priateAptModule

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LikeService } from './like.service';
 import { LikeController } from './like.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Like } from './entities/like';
+import { PrivateAptModule } from 'src/private-apt/private-apt.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Like]), PrivateAptModule],
   providers: [LikeService],
-  controllers: [LikeController]
+  controllers: [LikeController],
 })
 export class LikeModule {}
