d7d9af8fb434bfea35fe34b6d68dfdeb6d49f058
knowave <kwaktaemin98@gmail.com>
2024-02-19 15:37:45 +0900

    add private apt

--- src/private-apt/private-apt.module.ts
+++ src/private-apt/private-apt.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrivateAptService } from './private-apt.service';
 import { PrivateAptController } from './private-apt.controller';
+import { PrivateAptRepository } from './private-apt.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PrivateApt } from './entities/private-apt.entity';
 
 @Module({
-  providers: [PrivateAptService],
+  imports: [TypeOrmModule.forFeature([PrivateApt])],
+  providers: [PrivateAptService, PrivateAptRepository],
   controllers: [PrivateAptController],
+  exports: [PrivateAptRepository],
 })
 export class PrivateAptModule {}
