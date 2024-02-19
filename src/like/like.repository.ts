4d57a0c2f043c86cd1fdf5da16f7eb42474fe159
knowave <kwaktaemin98@gmail.com>
2024-02-19 14:50:28 +0900

    getLikeByIdAndUserId: 좋아요 조회 코드 수정

--- src/like/like.repository.ts
+++ src/like/like.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Like } from './entities/like';
+import { GetLikeByIdAndUserIdDto } from './dto/get-like-by-id-and-user-id.dto';
 
 @Injectable()
 export class LikeRepository {
@@ -9,11 +10,29 @@ export class LikeRepository {
     @InjectRepository(Like) private readonly repository: Repository<Like>,
   ) {}
 
-  async getLikeByIdAndUserId(likeId: string, userId: string): Promise<Like> {
-    return await this.repository
+  async getLikeByIdAndUserId(
+    getLikeByIdAndUserIdDto: GetLikeByIdAndUserIdDto,
+  ): Promise<Like> {
+    const { likeId, userId, pblancNo, panId } = getLikeByIdAndUserIdDto;
+    const qb = this.repository
       .createQueryBuilder('like')
       .where('like.likeId = :likeId', { likeId })
-      .andWhere('like.user.id = :userId', { userId })
-      .getOne();
+      .andWhere('like.user.id = :userId', { userId });
+
+    if (pblancNo) {
+      qb.leftJoinAndSelect('like.privateApt', 'privateApt').andWhere(
+        'privateApt.id = :pblancNo',
+        { pblancNo },
+      );
+    }
+
+    if (panId) {
+      qb.leftJoinAndSelect('like.pubNotice', 'pubNotice').andWhere(
+        'pubNotice.id = :panId',
+        { panId },
+      );
+    }
+
+    return qb.getOne();
   }
 }
