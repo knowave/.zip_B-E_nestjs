43adba3866f974db3d1c832c05b8f6726c87a17e
knowave <kwaktaemin98@gmail.com>
2025-02-21 11:00:02 +0900

    auth decorators 생성

---
+++ src/common/decorators/current-user.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { CurrentUserType } from '../types/current-user.type';
+
+export const CurrentUser = createParamDecorator(
+  (_, ctx: ExecutionContext): CurrentUserType => {
+    const { user } = ctx.switchToHttp().getRequest();
+    return {
+      id: user.id,
+      email: user.email,
+      nickname: user.nickname,
+      isDeleted: user.isDeleted,
+    };
+  },
+);
