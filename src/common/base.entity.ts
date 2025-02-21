378d7fae336921ef23274d10b04c1ccbd5ea5d07
knowave <kwaktaemin98@gmail.com>
2025-02-21 13:12:41 +0900

    api property 설정

--- src/common/base.entity.ts
+++ src/common/base.entity.ts
@@ -1,3 +1,5 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 import {
   BeforeInsert,
   BeforeSoftRemove,
@@ -9,15 +11,20 @@ import {
 } from 'typeorm';
 
 export class BaseEntity {
+  @Expose()
+  @ApiProperty({ readOnly: true, description: '엔티티 고유 식별자' })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({ readOnly: true, description: '엔티티 생성 일시' })
   @CreateDateColumn()
   createdAt: Date;
 
+  @ApiProperty({ readOnly: true, description: '엔티티 수정 일시' })
   @UpdateDateColumn()
   updatedAt: Date;
 
+  @ApiProperty({ readOnly: true, description: '엔티티 삭제 일시' })
   @DeleteDateColumn()
   deletedAt: Date;
 
--- src/domains/user/entities/user.entity.ts
+++ src/domains/user/entities/user.entity.ts
@@ -2,47 +2,73 @@ import { BaseEntity } from 'src/common/base.entity';
 import { Column, Entity, OneToMany } from 'typeorm';
 import { Comment } from 'src/domains/comment/entities/comment.entity';
 import { Like } from 'src/domains/like/entities/like.entity';
+import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 @Entity()
 export class User extends BaseEntity {
+  @Expose()
+  @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty({ required: true, description: '사용자 email' })
   @Column({ type: 'varchar', unique: true, comment: '사용자 email' })
   email: string;
 
+  @Expose()
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ required: true, description: '사용자 닉네임' })
   @Column({ type: 'varchar', comment: '사용자 닉네임' })
   nickname: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ required: true, description: '사용자 비밀번호' })
   @Column({
     type: 'varchar',
     nullable: true,
     select: false,
     comment: '사용자 비밀번호',
   })
-  password: string;
+  password?: string;
 
+  @ApiProperty({ description: '사용자 refresh token' })
   @Column({
     type: 'varchar',
     length: 512,
     nullable: true,
     comment: '사용자 refresh token',
   })
-  token: string;
+  token?: string;
 
+  @Expose()
+  @IsString()
+  @ApiProperty({
+    description: '사용자가 관심있는 지역 (시/도)',
+  })
   @Column({
     type: 'varchar',
     length: 20,
     nullable: true,
     comment: '사용자가 관심있는 지역 (시/도)',
   })
-  region: string;
+  region?: string;
 
+  @Expose()
+  @IsString()
+  @ApiProperty({ description: '사용자 프로필 이미지 URL' })
   @Column({
     type: 'varchar',
     length: 2048,
     comment: '사용자 프로필 이미지 URL',
     nullable: true,
   })
-  imageUrl: string;
+  imageUrl?: string;
 
+  @Expose()
+  @IsBoolean()
+  @ApiProperty({ readOnly: true, description: '사용자 탈퇴 여부' })
   @Column({ type: 'boolean', default: false, comment: '사용자 탈퇴 여부' })
   isDeleted: boolean;
 
