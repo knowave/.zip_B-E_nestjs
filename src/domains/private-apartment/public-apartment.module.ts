f476821dcd89d4c80270a351852fb54d66869fbb
knowave <kwaktaemin98@gmail.com>
2025-02-24 11:37:45 +0900

    typeorm module for feature

--- src/domains/private-apartment/public-apartment.module.ts
+++ src/domains/private-apartment/public-apartment.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PublicApartmentRepository } from './public-apartment.repository';
 import { PublicApartmentService } from './public-apartment.service';
 import { PublicApartmentController } from './public-apartment.controller';
+import { PublicApartmentImage } from './entities/public-apartment-image.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([PublicApartmentRepository])],
+    imports: [TypeOrmModule.forFeature([PublicApartmentRepository, PublicApartmentImage])],
     providers: [PublicApartmentService, PublicApartmentRepository],
     controllers: [PublicApartmentController],
 })
