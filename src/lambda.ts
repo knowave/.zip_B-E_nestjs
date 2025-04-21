1c9f82799e720329f8d142e3e225bcaccb889416
knowave <kwaktaemin98@gmail.com>
2025-04-21 17:37:55 +0900

    lambda 배포 추가

---
+++ src/lambda.ts
@@ -0,0 +1,29 @@
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './domains/app.module';
+import { NODE_ENV, ORIGIN_URL, PORT } from './common/env';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './common/config/swagger.config';
+import { Handler } from 'aws-lambda';
+import serverlessExpress from '@codegenie/serverless-express';
+import express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+let chachedHandler: Handler;
+
+async function bootstrap(): Promise<Handler> {
+    const expressApp = express();
+    const adapter = new ExpressAdapter(expressApp);
+    const app = await NestFactory.create(AppModule, adapter);
+
+    if (NODE_ENV !== 'prod') setupSwagger(app);
+
+    await app.init();
+
+    return serverlessExpress({ app: expressApp });
+}
+
+export const handler: Handler = async (event, context, callback) => {
+    if (!chachedHandler) chachedHandler = await bootstrap();
+
+    return chachedHandler(event, context, callback);
+};
