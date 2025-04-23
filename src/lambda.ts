ad5a151cd7684e13212f3948f66e90fa06fb5ef7
knowave <kwaktaemin98@gmail.com>
2025-04-23 12:43:38 +0900

    lambda api gateway base end point 수정

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,28 +1,32 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './domains/app.module';
-import { NODE_ENV } from './common/env';
-import { setupSwagger } from './common/config/swagger.config';
-import { Handler } from 'aws-lambda';
-import serverlessExpress from '@codegenie/serverless-express';
-import express from 'express';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+import { AppModule } from './domains/app.module';
+import express from 'express';
+import { configure as serverlessExpress } from '@codegenie/serverless-express';
 
-let chachedHandler: Handler;
+let cachedHandler: Handler;
 
 async function bootstrap(): Promise<Handler> {
     const expressApp = express();
+
+    expressApp.use((req, _, next) => {
+        const stagePrefix = '/dev';
+        if (req.url.startsWith(stagePrefix)) {
+            req.url = req.url.slice(stagePrefix.length) || '/';
+        }
+        next();
+    });
+
     const adapter = new ExpressAdapter(expressApp);
     const app = await NestFactory.create(AppModule, adapter);
-
-    if (NODE_ENV !== 'prod') setupSwagger(app);
 
     await app.init();
 
     return serverlessExpress({ app: expressApp });
 }
 
-export const handler: Handler = async (event, context, callback) => {
-    if (!chachedHandler) chachedHandler = await bootstrap();
-
-    return chachedHandler(event, context, callback);
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+    if (!cachedHandler) cachedHandler = await bootstrap();
+    return cachedHandler(event, context, callback);
 };
